c6cf808e86b24f24daba5c1dadcc1d471cdedac2
Samiaboug <bougtoubsamia@gmail.com>
2024-11-28 16:32:12 +0000

    tuto8

---
+++ ConceptsAvancesKotlin/tuto8/List/app/src/main/java/com/example/list/data/dataSource.kt
@@ -0,0 +1,10 @@
+package com.example.list.data
+
+import com.example.list.R
+import com.example.list.model.ListItem
+
+val sampleItems = listOf(
+    ListItem("Tâche 1", R.drawable.ic_task),
+    ListItem("Tâche 2", R.drawable.ic_task),
+    ListItem("Tâche 3", R.drawable.ic_task)
+)
